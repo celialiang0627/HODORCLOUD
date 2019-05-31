15c5a4126214d7daceef443cc147181f3f9d76df
SaulHE <xinheasde@gmail.com>
2019-05-31 10:06:12 +0800

    init files and the frameworks

---
+++ hodor-fe/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hodor-fe'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
